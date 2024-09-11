b2013b874214eae0859daabd7c6853e211bd402a
echavez <echavez@pkglobal.com>
2024-09-11 12:19:50 -0600

    docs creado

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // @ts-ignore
 import { GifExpertApp } from "./GifExpertApp";
-
+//https://www.netlify.com/
+//https://app.netlify.com/sites/gif-expert-henry/deploys/66e0e2e239d2e64b458add44
 import "./styles.css";
 
 
